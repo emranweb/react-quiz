be7b5e2dade07abebccccf73037ad25b0774277a
emranweb <emranbd77@gmail.com>
2022-03-17 11:22:00 +0600

    player

---
+++ src/components/Quiz.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Answers from "./Answers";
+import Progress from "./Progress";
+
+const Quiz = () => {
+  return (
+    <>
+      <h1>Pick three of your favorite Star Wars Flims</h1>
+      <h4>Question can have multiple answers</h4>
+      <Answers />
+      <Progress />
+    </>
+  );
+};
+
+export default Quiz;
--- src/pages/QuizPage.js
+++ src/pages/QuizPage.js
@@ -1,15 +1,10 @@
 import React from "react";
-import Answers from "../components/Answers";
 import Layout from "../layouts/Layout";
 
 const QuizPage = () => {
   return (
     <>
-      <Layout>
-        <h1>Pick three of your favorite Star Wars Flims</h1>
-        <h4>Question can have multiple answers</h4>
-        <Answers />
-      </Layout>
+      <Layout></Layout>
     </>
   );
 };
