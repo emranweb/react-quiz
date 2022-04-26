c8243b78113a38194ce01a554f73672d65114c4b
emranweb <emranbd77@gmail.com>
2022-04-26 19:18:08 +0600

    hooks

--- src/hook/useVideoList.js
+++ src/hook/useVideoList.js
@@ -1,16 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { useEffect } from "react";
-import { getDatabase, orderByKey, query, ref } from "firebase/database";
+import { get, getDatabase, orderByKey, query, ref } from "firebase/database";
 
 const useVideoList = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [videos, setVideos] = useState([]);
+
   useEffect(() => {
-    const fetchVideos = () => {
+    const fetchVideos = async () => {
       const db = getDatabase();
       const videoRef = ref(db, "videos");
       const videoQuery = query(videoRef, orderByKey);
+
+      try {
+        setLoading(true);
+        const snapshot = await get(videoQuery);
+        setLoading(false);
+
+        console.log(snapshot);
+
+        if (snapshot.exists()) {
+          console.log(snapshot);
+        }
+      } catch (error) {
+        console.log(error);
+      }
     };
+
+    fetchVideos();
   }, []);
-  return <div>useVideoList</div>;
+  return {
+    loading,
+    error,
+    videos,
+  };
 };
 
 export default useVideoList;
