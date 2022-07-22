57fbc91132378a27d266fd5f420d1a0ba6187622
emranweb <emranbd77@gmail.com>
2022-07-22 10:56:16 +0600

    mini button

--- src/components/MiniPlayer.js
+++ src/components/MiniPlayer.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import image from "../assets/3.jpg";
 import style from "../style/MiniPlayer.module.css";
+import ReactPlayer from "react-player";
 
 const MiniPlayer = () => {
+  const buttonRef = useRef();
+  const [state, setState] = useState(true);
+
+  const toggleButton = () => {
+    if (state) {
+      buttonRef.current.classList.add(style.floatingBtn);
+      setState(false);
+    } else {
+      buttonRef.current.classList.remove(style.floatingBtn);
+      setState(true);
+    }
+  };
+
   return (
-    <div className={`${style.miniPlayer} ${style.floatingBtn}`}>
+    <div
+      className={`${style.miniPlayer}`}
+      ref={buttonRef}
+      onClick={toggleButton}
+    >
       <span className={`material-icons-outlined ${style.open}`}>
-        {" "}
-        play_circle_filled{" "}
+        play_circle_filled
       </span>
       <span className={`material-icons-outlined ${style.close}`}> close </span>
       <img src={image} alt="" />
